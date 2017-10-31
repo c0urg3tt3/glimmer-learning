09ff1281e1c3c3b9944c866c9c8902dc3b812f55
pierre-arnaud lumalé <kadactivity@gmail.com>
2017-10-31 03:56:27 +0100

    set up project

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,25 @@
 'use strict';
 
+const cjs = require('rollup-plugin-commonjs')
+const md = require('rollup-plugin-md')
+const replace = require('rollup-plugin-replace')
 const GlimmerApp = require('@glimmer/application-pipeline').GlimmerApp;
 
 module.exports = function(defaults) {
   let app = new GlimmerApp(defaults, {
-    // Add options here
+    rollup: {
+      plugins: [
+        cjs({
+          include: /node_modules/
+        }),
+        md({
+          marked: {}
+        }),
+        replace({
+          "process.env.NODE_ENV": JSON.stringify(process.env.EMBER_ENV)
+        })
+      ]
+    }
   });
 
   return app.toTree();
